ccf0f2efe998d4627c9b9ed9c778157c9d1e8b18
h1nigami <timokhinivan1@gmail.com>
2023-11-12 00:40:46 +0300

    database

---
+++ Data/database_settings.py
@@ -0,0 +1,27 @@
+import aiosqlite
+
+class DB:
+    def __init__(self, db_name):
+        self.db_name = db_name
+        
+    async def create_tables(self):
+        async with aiosqlite.connect(self.db_name) as db:
+            await db.execute("""
+                CREATE TABLE IF NOT EXISTS users (
+                    id INTEGER PRIMARY KEY,
+                    user_id INTEGER,
+                    username TEXT,
+                    start_time datetime,
+                    end_time datetime,
+                    month_paid INTEGER,
+                    real_paid INTEGER
+                )
+            """)
+            await db.commit()
+    
+    async def get_user(self, tg_id:int):
+        async with aiosqlite.connect(self.db_name) as db:
+            cursor = await db.execute("SELECT * FROM users WHERE user_id = ?", (tg_id,))
+            return await cursor.fetchone()[0]
+        
+db = DB('database.db')
